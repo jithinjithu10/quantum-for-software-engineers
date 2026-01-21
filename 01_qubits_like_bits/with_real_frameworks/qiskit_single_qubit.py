4f3130f10c67d6babfb6c3b9ce231528ccf5d2ad
Jithin S <jithinyakkara@gmail.com>
2026-01-21 10:20:49 +0530

    Create qiskit_single_qubit.py

---
+++ 01_qubits_like_bits/with_real_frameworks/qiskit_single_qubit.py
@@ -0,0 +1,12 @@
+from qiskit import QuantumCircuit, Aer, execute
+
+qc = QuantumCircuit(1, 1)
+qc.h(0)            # create superposition
+qc.measure(0, 0)
+
+backend = Aer.get_backend("qasm_simulator")
+job = execute(qc, backend, shots=1000)
+result = job.result()
+
+counts = result.get_counts()
+print(counts)
