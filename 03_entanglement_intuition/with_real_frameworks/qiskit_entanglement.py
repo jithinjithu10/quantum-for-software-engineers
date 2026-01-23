50943c58b150444725e8df6cbc6d3f764b351498
Jithin S <jithinyakkara@gmail.com>
2026-01-23 10:24:44 +0530

    Add Qiskit entanglement example

---
+++ 03_entanglement_intuition/with_real_frameworks/qiskit_entanglement.py
@@ -0,0 +1,10 @@
+from qiskit import QuantumCircuit, Aer, execute
+
+qc = QuantumCircuit(2, 2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure([0, 1], [0, 1])
+
+backend = Aer.get_backend("qasm_simulator")
+job = execute(qc, backend, shots=1000)
+print(job.result().get_counts())
