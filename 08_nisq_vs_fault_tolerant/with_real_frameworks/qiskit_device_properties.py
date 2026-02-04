bc23afe004890182f5a6c5f1f6ceba390cba8bce
Jithin S <jithinyakkara@gmail.com>
2026-02-04 11:25:10 +0530

    Create qiskit_device_properties.py

---
+++ 08_nisq_vs_fault_tolerant/with_real_frameworks/qiskit_device_properties.py
@@ -0,0 +1,8 @@
+from qiskit import IBMQ
+
+print("Real devices expose:")
+print("- Number of qubits")
+print("- Gate errors")
+print("- Readout errors")
+print("- Coherence times")
+print("- Connectivity constraints")
